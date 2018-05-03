7b1f38b64c24bb23caf6481d5e59df5830fa8f99
Markus Mayer <widemeadows@gmail.com>
2018-05-03 02:51:52 +0200

    Update model

--- models.py
+++ models.py
@@ -27,24 +27,18 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         
-        self.conv1 = nn.Conv2d(1, 32, [5, 5])        ## output size = (W-F)/S +1 = (224-5)/1 +1 = 220
+        self.conv1 = nn.Conv2d(1, 32, 7, 3, 1)       ## output size = (W-F+2P)/S +1 = (224-7+2)/3 +1 = 74
+        self.conv2 = nn.Conv2d(32, 64, 5, 3, 0)      ## output size = (W-F+2P)/S +1 = (74-5)/3 +1 = 24
+        self.conv3 = nn.Conv2d(64, 128, 5, 3, 1)     ## output size = (24-5+2)/3 +1 = 8
+        self.conv4 = nn.Conv2d(128, 256, 3, 1, 0)    ## output size = (8-3)/1 +1 = 6
+        self.conv5 = nn.Conv2d(256, 512, 3, 1, 0)    ## output size = (6-3)/1 +1 = 4
+        self.conv6 = nn.Conv2d(512, 512, 1, 1, 0)    ## output size = (4-1)/1 +1 = 4
         
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-
-        self.pool = nn.MaxPool2d(2, 2)          ## output size = (W-F)/S +1 = (220-2)/2 +1 = 110
-        
-        self.conv2 = nn.Conv2d(32, 64, 3)       ## output size = (W-F)/S +1 = (110-3)/1 +1 = 108
-        # pool
-        self.conv3 = nn.Conv2d(64, 128, 3, 2)   ## output size = (W-F)/S +1 = (54-3)/2 +1 = 26
-        # pool
-        self.conv4 = nn.Conv2d(128, 256, 3, 2)  ## output size = (W-F)/S +1 = (13-3)/1 +1 = 6
-        # pool
-        self.conv5 = nn.Conv2d(256, 512, 3)     ## output size = (W-F)/S +1 = (3-3)/1 +1 = 1
-        
-        self.fc1 = nn.Linear(512, 512)
-        self.fc1_drop = nn.Dropout(p=0.4)
-        self.fc2 = nn.Linear(512, 136)
+        self.fc1 = nn.Linear(4*4*512, 1024)
+        self.fc1_drop = nn.Dropout(p=0.3)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.fc2_drop = nn.Dropout(p=0.3)
+        self.fc3 = nn.Linear(1024, 136)
         
 
         
@@ -54,17 +48,20 @@ class Net(nn.Module):
         ## x = self.pool(F.relu(self.conv1(x)))
         
         # Apply convolutional layers
-        x = self.pool(F.selu(self.conv1(x)))
-        x = self.pool(F.selu(self.conv2(x)))
-        x = self.pool(F.selu(self.conv3(x)))
-        x = self.pool(F.selu(self.conv4(x)))
+        x = F.selu(self.conv1(x))
+        x = F.selu(self.conv2(x))
+        x = F.selu(self.conv3(x))
+        x = F.selu(self.conv4(x))
         x = F.selu(self.conv5(x))
+        x = F.selu(self.conv6(x))
 
         # Flatten and continue with dense layers
         x = x.view(x.size(0), -1)
         x = F.selu(self.fc1(x))
         x = self.fc1_drop(x)
-        x = self.fc2(x)
+        x = F.selu(self.fc2(x))
+        x = self.fc2_drop(x)
+        x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
